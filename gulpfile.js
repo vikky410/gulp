9974e49818e21eb045c9368b648bf99c85bcc85e
vikky410 <vikky4fnds@gmail.com>
2016-07-25 23:38:59 +0530

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp     = require('gulp');
+var args     = require('yargs').argv;
+
+var $        = require('gulp-load-plugins')({lazy: true});
+
+gulp.task('vet', function () {
+    log('Analyzing code with JSHINT and JSCS');
+    return gulp
+    .src([
+    './src/**/*.js',
+    './*.js'
+    ])
+   .pipe($.if(args.verbose, $.print()))
+   .pipe($.jscs())
+   .pipe($.jshint())
+   .pipe($.jshint.reporter('jshint-stylish', {verbose: true}))
+   .pipe($.jshint.reporter('fail'));
+});
+
+function log(msg) {
+    if (typeof(msg) === 'object') {
+        for (var item in msg) {
+            if (msg.hasOwnProperty(item)) {
+                $.util.log($.util.colors.blue(msg[item]));
+            }
+        }
+    } else {
+        $.util.log($.util.colors.blue(msg));
+    }
+}
